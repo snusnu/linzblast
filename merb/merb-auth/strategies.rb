8cf56d8b2bc8df7d960af91a5f45dd38949973eb
snusnu <gamsnjaga@gmail.com>
2009-05-24 23:38:16 +0200

    added :invitation_code merb-auth strategy

--- merb/merb-auth/strategies.rb
+++ merb/merb-auth/strategies.rb
@@ -5,7 +5,31 @@
 #
 # To load and set the order of strategy processing
 
+# This strategy uses a login  and password parameter.
+#
+# Overwrite the :password_param, and :login_param
+# to return the name of the field (on the form) that you're using the 
+# login with.  These can be strings or symbols
+#
+# == Required
+#
+# === Methods
+# <User>.authenticate(login_param, password_param)
+#
+
 Merb::Slices::config[:"merb-auth-slice-password"][:no_default_strategies] = true
+
+class InvitationCode < Merb::Authentication::Strategy
+  
+  def run!
+    Code.get request.params[:invitation_code]
+  end
+  
+  def strategy_error_message
+    "wrong codez"
+  end
+  
+end
 
 Merb::Authentication.activate!(:default_password_form)
 Merb::Authentication.activate!(:default_basic_auth)
