1e781482d4d213923dc79db2c298cd2d7eb535dd
snusnu <gamsnjaga@gmail.com>
2009-06-03 14:24:15 +0200

    fixed scoping of codes to code_generations in admin controller

--- app/controllers/admin/codes.rb
+++ app/controllers/admin/codes.rb
@@ -6,8 +6,8 @@ module Admin
     
     def index
       @codes = if params[:code_generation_id]
-        @code_generation = CodeGeneration.first params[:code_generation_id].to_i
-        @code_generation.codes.all
+        @code_generation = CodeGeneration.get(params[:code_generation_id].to_i)
+        @code_generation.codes
       else
         Code.all
       end
