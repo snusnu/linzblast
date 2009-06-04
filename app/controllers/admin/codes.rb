9045e6724747e18bb9d8ae397c859df27116e1ad
snusnu <gamsnjaga@gmail.com>
2009-06-05 01:56:48 +0200

    revert usage of Resource.with_deleted
    * wait for http://is.gd/Oyhw

--- app/controllers/admin/codes.rb
+++ app/controllers/admin/codes.rb
@@ -5,13 +5,12 @@ module Admin
     layout :dashboard
     
     def index
-      Code.with_deleted do
-        @codes = if params[:code_generation_id]
-          @code_generation = CodeGeneration.get(params[:code_generation_id].to_i)
-          @code_generation.codes
-        else
-          Code.all
-        end
+      # FIXME wait for datamapper to properly support Resource.with_deleted
+      @codes = if params[:code_generation_id]
+        @code_generation = CodeGeneration.get(params[:code_generation_id].to_i)
+        @code_generation.codes
+      else
+        Code.all
       end
       display @codes
     end
