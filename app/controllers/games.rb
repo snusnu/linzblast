882ba278a98181549be64bd65f55fe61beb6fa0c
snusnu <gamsnjaga@gmail.com>
2009-06-04 04:41:23 +0200

    added create and update actions for the public games controller

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -14,4 +14,20 @@ class Games < Application
     display @game
   end
 
+  def create
+    @game = Game.new(params[:game])
+    if @game.save
+      redirect resource(:admin, @game)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @game = Game.get(params[:id])
+    raise NotFound unless @game
+    @game.update(params[:game])
+    redirect resource(:admin, @game, :edit)
+  end
+
 end
