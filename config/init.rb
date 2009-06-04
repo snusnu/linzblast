39c94ebdab2826b1fc929883a2db0f95fd12e1b8
snusnu <gamsnjaga@gmail.com>
2009-06-04 20:42:11 +0200

    added config option to allow unauthenticated games

--- config/init.rb
+++ config/init.rb
@@ -13,6 +13,9 @@ Merb::Config.use do |c|
   # cookie session store configuration
   c[:session_secret_key]  = 'ba9baa83815a89fd66eb6a91fcec4499202aed79'  # required for cookie session store
   c[:session_id_key] = '_linzblast_session_id' # cookie session id key, defaults to "_session_id"
+
+  # set to false if the game shouldn't be protected with invitation codes
+  c[:require_authentication] = true
 end
  
 Merb::BootLoader.before_app_loads do
