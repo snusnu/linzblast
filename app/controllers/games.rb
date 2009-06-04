f4e6f985ebbd8b76bb965a220d6ec2f80d3fddf8
snusnu <gamsnjaga@gmail.com>
2009-06-04 18:46:39 +0200

    better redirects and less actions for public games controller

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -11,23 +11,23 @@ class Games < Application
   
   def show
     @game = Game.get params[:id]
+    raise NotFound unless @game
+    display @game
+  end
+
+  def edit
+    @game = Game.get params[:id]
+    raise NotFound unless @game
     display @game
   end
 
   def create
     @game = Game.new(params[:game])
     if @game.save
-      redirect resource(:admin, @game)
+      redirect resource(@game, :edit)
     else
       render :new
     end
-  end
-
-  def update
-    @game = Game.get(params[:id])
-    raise NotFound unless @game
-    @game.update(params[:game])
-    redirect resource(:admin, @game, :edit)
   end
 
 end
