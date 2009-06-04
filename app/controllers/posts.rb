92c8dd3d9e8bf30316b35c535b5ddde3d9485b2c
snusnu <gamsnjaga@gmail.com>
2009-06-04 18:46:15 +0200

    removed unwanted public actions from posts controller

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,21 +1,4 @@
 class Posts < Application
-
-  def index
-    provides :html, :json
-    @posts = Post.all
-    display @posts
-  end
-  
-  def show
-    provides :html, :json
-    @post = Post.get params[:id]
-    display @post
-  end
-
-  def new
-    @post = Post.new
-    display @post
-  end
   
   def create
     @post = Post.new(params[:post])
