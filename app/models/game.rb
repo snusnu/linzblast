70b76aea9ac82535b318ddb6775f72c43bbd3a28
snusnu <gamsnjaga@gmail.com>
2009-06-04 23:19:14 +0200

    perform access control in games not in posts

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,17 +4,58 @@ class Game
   
   # properties
   
-  property :id,         Serial
+  property :id,          Serial
   
-  property :code_id,    Integer, :nullable => false
-  property :stage_id,   Integer, :nullable => false
+  property :stage_id,    Integer, :nullable => false
   
-  property :created_at, DateTime
-  property :updated_at, DateTime
+  property :nr_of_posts, Integer, :nullable => false, :default => 0
+  
+  property :created_at,  DateTime
+  property :updated_at,  DateTime
   
   # associations
   
-  belongs_to :code
   belongs_to :stage
+  
+  
+  # virtual attributes
+  
+  def code=(secret)
+    @code = Code.first(:secret => secret)
+  end
+  
+  def code
+    @code
+  end
+  
+  
+  # validation
+  
+  def valid?
+    stage.access_restricted ? !code.nil? : true
+  end
+  
+  after :save, :stop_game_if_necessary
+  
+  
+  # public api
+  
+  def save(context = :default)
+    super
+  end
+  
+  
+  protected
+    
+  # after filter
+  def stop_game_if_necessary(*args)
+    if stage.access_restricted? && no_posts_left?
+      code.destroy
+    end
+  end
+  
+  def no_posts_left?
+    nr_of_posts == code.nr_of_posts
+  end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post
   
   include DataMapper::Resource
   
+  # properties
+  
   property :id,               Serial
   
   property :stage_id,         Integer, :nullable => false
@@ -12,69 +14,34 @@ class Post
   property :text_color,       String,  :length => 6, :default => "FFFFFF"
   property :background_color, String,  :length => 6, :default => "FFFFFF"
   
-  property :x_coord,          Integer
-  property :y_coord,          Integer
+  property :x_coord,          Integer, :nullable => false
+  property :y_coord,          Integer, :nullable => false
   
   property :polygon,          Object
   
   property :created_at,       DateTime
   property :updated_at,       DateTime
   
+  # associations
   
   belongs_to :stage
   belongs_to :style
   
+  # delegations
   
   delegate :name,        :to => :style, :prefix => true
   delegate :description, :to => :style, :prefix => true
   delegate :impact,      :to => :style, :prefix => true
   delegate :ttl,         :to => :style, :prefix => true
   
-  
-  before :save, :check_invitation_code!
-  after  :save, :destroy_invitation_code!
-  
-  
-  def ready?
-    !new? && valid_invitation_code? && valid_coordinates?
-  end
-  
-  def valid_coordinates?
-    !self.x_coord.nil? && !self.y_coord.nil?
-  end
-  
-  def valid_invitation_code?
-    !@invitation_code.nil?
-  end
-  
-  def invitation_code=(code)
-    @invitation_code = Code.first(:secret => code)
-    initialize_style_container! if @invitation_code
-    @invitation_code
-  end
-  
-  def invitation_code
-    @invitation_code
-  end
-  
-  
+
+  before :save, :initialize_style_container
+
   protected
   
-  # before filter
-  def check_invitation_code!(*args)
-    throw :halt  if new? && !valid_invitation_code?
-  end
-    
-  # after filter
-  def destroy_invitation_code!(*args)
-    invitation_code.destroy if ready?
-  end
-  
-  
-  private
-  
-  def initialize_style_container!
-    if new? && valid_invitation_code?
+  # before save hook
+  def initialize_style_container
+    if new?
       self.polygon = style.generate_container
     end
   end
